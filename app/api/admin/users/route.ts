31cc853451a75db9d5f2308968604a6b78e02cce
milliondreamsblog <akshatsan23@gmail.com>
2026-02-07 14:03:53 +0530

    Selective merge from PR-5 (exclude landing page)

--- app/api/admin/users/route.ts
+++ app/api/admin/users/route.ts
@@ -4,6 +4,8 @@ import { cookies } from "next/headers";
 import connectDB from "@/lib/mongodb";
 import AuthUser from "@/app/models/AuthUser";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     try {
         await connectDB();
